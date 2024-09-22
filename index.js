96a378a610a9ea8cc4470adfb2d0ccc156f7186f
CYBERGAMER9 <143714358+CYBERGAMER9@users.noreply.github.com>
2024-09-22 14:10:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,10 @@ client.on('ready', async () => {
         .setURL('https://twitch.tv/cyber_winner')
         .setState('Idle')
         .setName('Chill with cyber')
-        .setDetails('CHILL WITH CYBER')
-        .setAssetsLargeImage('https://media.discordapp.net/attachments/1169491681507278863/1209391005648818217/oshi-no-ko-idol.gif?ex=65e6c025&is=65d44b25&hm=dfb968338ef8960d077ef6b9bf6a096a64f294b051f814d50cf5247b8a67e41b&')
+        .setDetails('CHILL WITH PRANJAL')
+      
         .setAssetsLargeText('WATCHING YOUTUBE')
-        .setAssetsSmallImage('https://cdn.discordapp.com/emojis/1101758417720328203.webp?size=96&quality=lossless')
-        .setAssetsSmallText('Verified')
-        .addButton('DISCORD BOT', 'https://discord.com/api/oauth2/authorize?client_id=1174640185380716554&permissions=8&scope=bot+applications.commands')
-        .addButton('Discord Server', 'https://discord.com/invite/K99nfhCyHx');
+        .setAssetsSmallText('Verified');
 
     client.user.setActivity(r);
     client.user.setPresence({ status: 'dnd' });
